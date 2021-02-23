29d5270c00c35687240706bd8bf5edb7b379aa63
Luke Jacobs <lukedjacobs@Yahoo.com>
2021-02-23 14:34:02 -0600

    First commit

---
+++ counter.sv
@@ -0,0 +1,23 @@
+
+module counter(
+	input logic increment, 
+	input logic reset,
+	input logic clk,
+	output logic [3:0]
+);
+
+logic input [3:0] next_state;
+
+always_comb
+	if (increment)
+		next_state = 
+end
+
+
+always_ff
+
+end
+
+
+
+endmodule
---
+++ multiplier_fsm.sv
@@ -0,0 +1,21 @@
+
+module multiplier_fsm(
+	input logic reset,
+	input logic clk,
+	input logic run,
+	input logic clearA_loadB,
+	output logic shift,
+	output logic add,
+	output logic sub
+);
+
+always_comb
+
+end
+
+always_ff
+
+end
+
+
+endmodule
